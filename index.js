ad7a4000e0a306482dab91f8abc18e9523b62383
Andrija Petrovic <andrija.hers@gmail.com>
2021-04-22 13:09:59 +0000

    ECONNRESET headache

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var fs = require('fs'),
 
 function removePipe(filename){
   if(fs.existsSync(filename)){
+    console.log('pipe cleaner unlinking', filename);
     fs.unlinkSync(filename);
   }
 }
@@ -11,8 +12,18 @@ function checkForUnixSocketResidue(filename, exc){
   if (os.type() === 'Windows_NT') {
     return;
   }
-  console.error(exc.stack);
-  console.log(process.pid, 'cleaning pipes because of',exc);
+  if (exc.code === 'ECONNRESET') { //nodejs cannot handle this properly, even with all the handlers in place
+    var stack = new Error().stack;
+    var stackarr = stack.split('\n');
+    if (stackarr.length<5) {
+      console.log("pipe cleaner will ignore ECONNRESET in nodejs 14 because it doesn't handle this error properly, even with all the handlers in place");
+      return;
+    }
+    console.log('pipe cleaner will NOT ignore ECONNRESET in nodejs 14 because the stack trace was deeper than 4', stack);
+  }
+  if (exc) {
+    console.log(process.pid, 'pipe cleaner got', exc);
+  }
   if(typeof filename === 'object' && filename instanceof Array){
     filename.forEach(removePipe);
   }else{
