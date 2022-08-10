b6f8fe3184ae0e066db953af2fc4563f855f3fb4
Andrija Petrovic <andrija.hers@gmail.com>
2022-08-10 12:12:03 +0000

    More security

--- index.js
+++ index.js
@@ -12,17 +12,21 @@ function checkForUnixSocketResidue(filename, exc){
   if (os.type() === 'Windows_NT') {
     return;
   }
-  if (exc.code === 'ECONNRESET') { //nodejs cannot handle this properly, even with all the handlers in place
-    var stack = new Error().stack;
-    var stackarr = stack.split('\n');
-    if (stackarr.length<5) {
-      console.log("pipe cleaner will ignore ECONNRESET in nodejs 14 because it doesn't handle this error properly, even with all the handlers in place");
-      return;
-    }
-    console.log('pipe cleaner will NOT ignore ECONNRESET in nodejs 14 because the stack trace was deeper than 4', stack);
+  if (!exc) {
+    console.trace();
+    console.error('No exception in shouldClose?');
   }
   if (exc) {
     //console.log(process.pid, 'pipe cleaner got', exc);
+    if (exc.code === 'ECONNRESET') { //nodejs cannot handle this properly, even with all the handlers in place
+      var stack = new Error().stack;
+      var stackarr = stack.split('\n');
+      if (stackarr.length<5) {
+        console.log("pipe cleaner will ignore ECONNRESET in nodejs 14 because it doesn't handle this error properly, even with all the handlers in place");
+        return;
+      }
+      console.log('pipe cleaner will NOT ignore ECONNRESET in nodejs 14 because the stack trace was deeper than 4', stack);
+    }
   }
   if(typeof filename === 'object' && filename instanceof Array){
     filename.forEach(removePipe);
